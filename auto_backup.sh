c5142ab4cb2ba1660a520ae9ff6313e00fe431a5
MrRad1st <Radin1383rvn@gmail.com>
2025-07-27 15:41:30 +0330

    add needed arguments for main functionality

--- auto_backup.sh
+++ auto_backup.sh
@@ -10,3 +10,12 @@ if [ "$#" -lt 2 ]; then
 fi
 
 
+# Arguments
+BACKUP_DIR=$1
+FILE_EXT=$2
+DAYS_TO_KEEP=7
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+DEST="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
+CONF_FILE="backup.conf"
+LOG_FILE="backup.log"
+
